e1db2a3878e3821e29e296cb650c748325323cb1
leaf <leaf@local>
2024-08-16 19:08:37 +0400

    Initial commit

---
+++ src/main/java/com/spork/App.java
@@ -0,0 +1,13 @@
+package com.spork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
